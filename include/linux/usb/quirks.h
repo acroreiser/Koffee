a31fbe99cb937ff263378e86e2c7b9b9ec926782
Johan Hovold <johan@kernel.org>
2014-08-25 17:51:26 +0200

    USB: core: add device-qualifier quirk
    commit 2a159389bf5d962359349a76827b2f683276a1c7 upstream.
    
    Add new quirk for devices that cannot handle requests for the
    device_qualifier descriptor.
    
    A USB-2.0 compliant device must respond to requests for the
    device_qualifier descriptor (even if it's with a request error), but at
    least one device is known to misbehave after such a request.
    
    Suggested-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- include/linux/usb/quirks.h
+++ include/linux/usb/quirks.h
@@ -42,4 +42,7 @@
 /* resume bus driver after dpm resume  */
 #define USB_QUIRK_NO_DPM_RESUME         0x00000400
 
+/* device can't handle device_qualifier descriptor requests */
+#define USB_QUIRK_DEVICE_QUALIFIER	0x00000100
+
 #endif /* __LINUX_USB_QUIRKS_H */
