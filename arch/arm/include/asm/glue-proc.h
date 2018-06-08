5681410158b6d982aa7124ea60b76edb3f13d2b1
Jaroslav Zvezda <acroreiser@gmail.com>
2018-06-08 09:51:54 +0300

    Revert "ARM: 8103/1: save/restore Cortex-A9 CP15 registers on suspend/resume"
    This reverts commit 484c1c478e004067ea5f38e0cd67fd85cad457fa.

--- arch/arm/include/asm/glue-proc.h
+++ arch/arm/include/asm/glue-proc.h
@@ -240,12 +240,12 @@
 #endif
 
 #ifdef CONFIG_CPU_V7
-/*
- * Cortex-A9 needs a different suspend/resume function, so we need
- * multiple CPU support for ARMv7 anyway.
- */
+# ifdef CPU_NAME
 #  undef  MULTI_CPU
 #  define MULTI_CPU
+# else
+#  define CPU_NAME cpu_v7
+# endif
 #endif
 
 #ifndef MULTI_CPU
