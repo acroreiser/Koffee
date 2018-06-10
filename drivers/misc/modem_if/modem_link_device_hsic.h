f086062e04c8b23fd9bbed22f2f553269f30f7d2
Jaroslav Zvezda <acroreiser@gmail.com>
2018-06-10 18:17:59 +0300

    Revert "modem_if: fixup v4"
    This reverts commit 68f64c9cbc79ca6dcd9276bab2a3588587a632c7.

--- drivers/misc/modem_if/modem_link_device_hsic.h
+++ drivers/misc/modem_if/modem_link_device_hsic.h
@@ -92,6 +92,7 @@ struct link_pm_data {
 	bool link_pm_active;
 	int resume_retry_cnt;
 
+	struct wake_lock l2_wake;
 	struct wake_lock boot_wake;
 	struct wake_lock rpm_wake;
 	struct wake_lock tx_async_wake;
