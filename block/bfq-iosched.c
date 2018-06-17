a3ea98de88045ae62f501d45e52a044034d77107
Jaroslav Zvezda <acroreiser@gmail.com>
2018-06-17 16:12:14 +0300

    block: bfq: more realtime efforts

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -70,7 +70,7 @@
 #include "bfq.h"
 
 /* Max number of dispatches in one round of service. */
-static const int bfq_quantum = 4;
+static const int bfq_quantum = 2;
 
 /* Expiration time of sync (0) and async (1) requests, in jiffies. */
 static const int bfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
@@ -82,11 +82,11 @@ static const int bfq_back_max = 16 * 1024;
 static const int bfq_back_penalty = 2;
 
 /* Idling period duration, in jiffies. */
-static int bfq_slice_idle = HZ / 125;
+static int bfq_slice_idle = 0;
 
 /* Default maximum budget values, in sectors and number of requests. */
-static const int bfq_default_max_budget = 16 * 1024;
-static const int bfq_max_budget_async_rq = 4;
+static const int bfq_default_max_budget = 8 * 1024;
+static const int bfq_max_budget_async_rq = 2;
 
 /*
  * Async to sync throughput distribution is controlled as follows:
@@ -96,7 +96,7 @@ static const int bfq_max_budget_async_rq = 4;
 static const int bfq_async_charge_factor = 10;
 
 /* Default timeout values, in jiffies, approximating CFQ defaults. */
-static const int bfq_timeout_sync = HZ / 8;
+static const int bfq_timeout_sync = HZ / 16;
 static int bfq_timeout_async = HZ / 25;
 
 struct kmem_cache *bfq_pool;
