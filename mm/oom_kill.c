1642ea5a5ad1392024c245aa422ce892b8f678f1
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-01 11:59:33 +0300

    mm: oom_kill: make more processes killable

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -161,13 +161,6 @@ static bool oom_unkillable_task(struct task_struct *p,
 	if (p->flags & PF_KTHREAD)
 		return true;
 
-	/* When mem_cgroup_out_of_memory() and p is not member of the group */
-	if (mem && !task_in_mem_cgroup(p, mem))
-		return true;
-
-	/* p may not have freeable memory in nodemask */
-	if (!has_intersects_mems_allowed(p, nodemask))
-		return true;
 
 	return false;
 }
