5d3d4af93e0b28fd9fa16c57f8556d777b0970a7
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-15 03:35:28 +0300

    koffee.sh: set SELinux contexts

--- anykernel_boeffla-i9300/ramdisk/koffee.sh
+++ anykernel_boeffla-i9300/ramdisk/koffee.sh
@@ -8,6 +8,8 @@
 /sbin/busybox mkdir /libs
 /sbin/busybox mount -t tmpfs tmpfs /libs
 /sbin/busybox mv /res/koffee/* /libs
+/sbin/busybox chmod 0644 /libs/*.so
+/sbin/busybox chcon -t system_file -r object_r /libs/*.so
 /sbin/busybox mount -o bind /libs/gralloc.exynos4.so /system/lib/hw/gralloc.exynos4.so
 /sbin/busybox mount -o bind /libs/hwcomposer.exynos4.so /system/lib/hw/hwcomposer.exynos4.so
 
