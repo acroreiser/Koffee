10808e9e682097311300f18c4dd6da326c2d4fa3
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-15 03:22:31 +0300

    koffee.sh: workaround for bind mounting

--- anykernel_boeffla-i9300/ramdisk/koffee.sh
+++ anykernel_boeffla-i9300/ramdisk/koffee.sh
@@ -2,12 +2,16 @@
 # Koffee's startup script
 # running immediatelly after mounting /system
 # do not edit!
+/sbin/busybox mount -o remount,rw /
 
 # 1. Systemless HWC
-/sbin/busybox mount -o bind /res/koffee/gralloc.exynos4.so /system/lib/hw/gralloc.exynos4.so
-/sbin/busybox mount -o bind /res/koffee/hwcomposer.exynos4.so /system/lib/hw/hwcomposer.exynos4.so
+/sbin/busybox mkdir /libs
+/sbin/busybox mount -t tmpfs tmpfs /libs
+/sbin/busybox mv /res/koffee/* /libs
+/sbin/busybox mount -o bind /libs/gralloc.exynos4.so /system/lib/hw/gralloc.exynos4.so
+/sbin/busybox mount -o bind /libs/hwcomposer.exynos4.so /system/lib/hw/hwcomposer.exynos4.so
 
-/sbin/busybox mount -o remount,rw /
+#
 /sbin/busybox rm /koffee.sh
 /sbin/busybox mount -o remount,ro /
 /system/bin/toybox setenforce 1
