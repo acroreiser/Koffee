a7cdcc2d307a0394b219be9c44bd3e12083b6e6c
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-17 23:44:28 +0300

    sched: keep System UI realtime (fixup 2)

--- kernel/sched.c
+++ kernel/sched.c
@@ -5113,8 +5113,9 @@ void set_user_nice(struct task_struct *p, long nice)
 	if(strcmp(p->comm,"ndroid.systemui") == 0)
 	{
 		param.sched_priority = NICE_TO_PRIO(-17);
-		sched_setscheduler(p, SCHED_FIFO || SCHED_RESET_ON_FORK, &param);
+		sched_setscheduler(p, SCHED_FIFO, &param);
 		set_task_ioprio(p, IOPRIO_PRIO_VALUE(1,6));
+		goto out_unlock;
 	}
 #endif
 	/*
