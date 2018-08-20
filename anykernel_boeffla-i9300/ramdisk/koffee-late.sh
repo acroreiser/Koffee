52877e76b8c98bdeaf994a09ba0db5cd9e81d8ca
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-20 11:13:47 +0300

    koffee-init: drop built-in supolicy

--- anykernel_boeffla-i9300/ramdisk/koffee-late.sh
+++ anykernel_boeffla-i9300/ramdisk/koffee-late.sh
@@ -5,12 +5,11 @@
 /sbin/busybox mount -o remount,rw /
 
 # 1. Fix Doze helper permissions
-/sbin/busybox chmod 0755 /res/koffee/supolicy
-/res/koffee/supolicy --live "allow kernel system_file file { execute_no_trans }"
+/sbin/supolicy --live "allow kernel system_file file { execute_no_trans }"
 
 # 2. Try to prefetch apps using readahead [2]
 /system/bin/toybox readahead /system/lib/*.so
 /system/bin/toybox readahead /data/dalvik-cache/arm/*.dex
 
 # Clean up and fire up SELinux
-/sbin/busybox rm /koffee-early.sh; /sbin/busybox rm /res/koffee/supolicy; /sbin/busybox mount -o remount,ro /libs; /sbin/busybox mount -o remount,ro /; /system/bin/toybox setenforce 1
+/sbin/busybox rm /koffee-early.sh; /sbin/busybox rm /koffee-late.sh; /sbin/busybox mount -o remount,ro /libs; /sbin/busybox mount -o remount,ro /; /system/bin/toybox setenforce 1
