b640eef5599453720fbf9451f01ff57077eea98e
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-03 17:32:14 +0300

    Revert "perf: Fix race in swevent hash"
    This reverts commit d64369a48249288455e8a721c7db9c70006edeaa.
    
    Change-Id: I0643f7ad8b8d867e4cc01fe3dd82faa5d4eaafa3

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5370,6 +5370,7 @@ static int swevent_hlist_get_cpu(struct perf_event *event, int cpu)
 	int err = 0;
 
 	mutex_lock(&swhash->hlist_mutex);
+
 	if (!swevent_hlist_deref(swhash) && cpu_online(cpu)) {
 		struct swevent_hlist *hlist;
 
@@ -7259,6 +7260,12 @@ static void perf_event_exit_cpu_context(int cpu)
 
 static void perf_event_exit_cpu(int cpu)
 {
+	struct swevent_htable *swhash = &per_cpu(swevent_htable, cpu);
+
+	mutex_lock(&swhash->hlist_mutex);
+	swevent_hlist_release(swhash);
+	mutex_unlock(&swhash->hlist_mutex);
+
 	perf_event_exit_cpu_context(cpu);
 }
 #else
