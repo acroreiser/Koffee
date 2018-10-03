41a2cf23b93e9b90af09842aa3e76588b62a54a1
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-03 15:06:37 +0300

    Revert "Linux 3.0.60"
    Change-Id: I6db707d25a86e1c369fc9bc824c88d21604bc1c5

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -3460,7 +3460,7 @@ static int ftrace_module_notify(struct notifier_block *self,
 
 struct notifier_block ftrace_module_nb = {
 	.notifier_call = ftrace_module_notify,
-	.priority = INT_MAX,	/* Run before anything that can use kprobes */
+	.priority = 0,
 };
 
 extern unsigned long __start_mcount_loc[];
