1c6997c0bd6f0d173c73abb70df2a71ef981433a
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-04 15:00:01 +0300

    Revert "dm table: share target argument parsing functions"
    This reverts commit fc90060e9e62815bec64cb9dc1a3221ab70737a3.
    
    Change-Id: Ib94acab3ece40922a47791b943475ce5fe13b8ed

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -30,6 +30,7 @@
 #include <linux/device-mapper.h>
 
 #define DM_MSG_PREFIX "crypt"
+#define MESG_STR(x) x, sizeof(x)
 
 /*
  * context holding the current state of a multi-part conversion
@@ -1778,12 +1779,12 @@ static int crypt_message(struct dm_target *ti, unsigned argc, char **argv)
 	if (argc < 2)
 		goto error;
 
-	if (!strcasecmp(argv[0], "key")) {
+	if (!strnicmp(argv[0], MESG_STR("key"))) {
 		if (!test_bit(DM_CRYPT_SUSPENDED, &cc->flags)) {
 			DMWARN("not suspended during key manipulation.");
 			return -EINVAL;
 		}
-		if (argc == 3 && !strcasecmp(argv[1], "set")) {
+		if (argc == 3 && !strnicmp(argv[1], MESG_STR("set"))) {
 			ret = crypt_set_key(cc, argv[2]);
 			if (ret)
 				return ret;
@@ -1791,7 +1792,7 @@ static int crypt_message(struct dm_target *ti, unsigned argc, char **argv)
 				ret = cc->iv_gen_ops->init(cc);
 			return ret;
 		}
-		if (argc == 2 && !strcasecmp(argv[1], "wipe")) {
+		if (argc == 2 && !strnicmp(argv[1], MESG_STR("wipe"))) {
 			if (cc->iv_gen_ops && cc->iv_gen_ops->wipe) {
 				ret = cc->iv_gen_ops->wipe(cc);
 				if (ret)
