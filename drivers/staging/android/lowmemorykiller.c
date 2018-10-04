41821c3de313f2e58840e80d422a0d9c347db19a
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-04 17:05:38 +0300

    Revert "staging: android/lowmemorykiller: Don't grab tasklist_lock"
    This reverts commit e6edd4723129b31402ad2da3d579abfaf5286f4d.
    
    Change-Id: I316183148e4c02cd4abe2b61b51a5854ee2ec94f

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -34,7 +34,6 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
-#include <linux/rcupdate.h>
 #include <linux/notifier.h>
 #define ENHANCED_LMK_ROUTINE
 
@@ -179,7 +178,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	selected_oom_adj = min_adj;
 #endif
 
-	rcu_read_lock();
+	read_lock(&tasklist_lock);
 	for_each_process(p) {
 		struct mm_struct *mm;
 		struct signal_struct *sig;
@@ -274,13 +273,13 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			     selected_oom_adj, selected_tasksize);
 		lowmem_deathpending = selected;
 		lowmem_deathpending_timeout = jiffies + HZ;
-		send_sig(SIGKILL, selected, 0);
+		force_sig(SIGKILL, selected);
 		rem -= selected_tasksize;
 	}
 #endif
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
-	rcu_read_unlock();
+	read_unlock(&tasklist_lock);
 	return rem;
 }
 
