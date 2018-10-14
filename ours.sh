ea91c10c3ea7f0b1c97051934db2633b16053a8f
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-14 17:23:52 +0300

    Revert "ours: remove mach-exynos / plat-samsung"
    This reverts commit 41e726ea165b30fb947cdf668d93189a6f6facec.
    
    Change-Id: Ia8400739932e86825647564bce48695e684cda65

--- ours.sh
+++ ours.sh
@@ -1,6 +1,6 @@
 xargs -L1 git checkout HEAD < ours.txt
 
-HARD_RESET="arch/arm/mach-s3c2410 arch/arm/mach-s3c64xx drivers/media sound include/sound drivers/usb drivers/regulator drivers/spi include/linux/regulator* include/linux/spi* drivers/gpio drivers/hwmon drivers/staging/zram" 
+HARD_RESET="arch/arm/mach-exynos arch/arm/mach-s3c2410 arch/arm/mach-s3c64xx arch/arm/mach-s5pv210 arch/arm/plat-s5p drivers/media sound include/sound drivers/usb drivers/regulator drivers/spi include/linux/regulator* include/linux/spi* drivers/gpio drivers/hwmon drivers/staging/zram" 
 
 echo $HARD_RESET | xargs -L1 rm -fr
 echo $HARD_RESET | xargs -L1 git checkout HEAD
