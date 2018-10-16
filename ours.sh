091b088ec545b292587e1fec6107660c21f533ee
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 16:46:36 +0300

    Update ours.sh
    Change-Id: Ibbd39d446245f18b22aa35d7a2d1f64db12650ac

--- ours.sh
+++ ours.sh
@@ -1,6 +1,6 @@
 xargs -L1 git checkout HEAD < ours.txt
 
-HARD_RESET="arch/arm/mach-exynos arch/arm/mach-s3c2410 arch/arm/mach-s3c64xx arch/arm/mach-s5pv210 arch/arm/plat-s5p drivers/media sound include/sound drivers/usb drivers/regulator drivers/spi include/linux/regulator* include/linux/spi* drivers/gpio drivers/hwmon drivers/staging/zram" 
+HARD_RESET="arch/arm/mach-exynos arch/arm/mach-s3c2410 arch/arm/mach-s3c64xx arch/arm/mach-s5pv210 arch/arm/plat-s5p drivers/media sound include/sound drivers/usb drivers/regulator drivers/spi include/linux/regulator* include/linux/spi* drivers/gpio drivers/hwmon drivers/staging/zram include/media drivers/staging/android drivers/input/touchscreen drivers/power include/linux/power* drivers/tty/serial/samsung*" 
 
 echo $HARD_RESET | xargs -L1 rm -fr
 echo $HARD_RESET | xargs -L1 git checkout HEAD
