c4d90b292236f5050cf714ba11e3e645b9bd53e2
Shilin Victor <chrono.monochrome@gmail.com>
2018-11-14 09:56:04 +0300

    mach-midas: fix derp
    Change-Id: Ifb1d8a8b993ebb5829b8a5eeb15864ce0facc813

--- arch/arm/mach-exynos/mach-midas.c
+++ arch/arm/mach-exynos/mach-midas.c
@@ -342,8 +342,6 @@ static struct spi_board_info spi2_board_info[] __initdata = {
 
 static struct i2c_board_info i2c_devs8_emul[];
 
-unsigned int lpcharge;
-
 int check_bootmode(void)
 {
         int inform2;
@@ -354,6 +352,7 @@ int check_bootmode(void)
         else
                 return 0;
 }
+
 
 #ifdef CONFIG_KEYBOARD_CYPRESS_TOUCH
 static void touchkey_init_hw(void)
@@ -3565,8 +3564,6 @@ static void __init midas_machine_init(void)
 #endif
 	__raw_writel((__raw_readl(EXYNOS4_CLKDIV_FSYS1) & 0xfff0fff0)
 		     | 0x80008, EXYNOS4_CLKDIV_FSYS1);
-
-	lpcharge = check_bootmode();
 }
 
 #ifdef CONFIG_EXYNOS_C2C
