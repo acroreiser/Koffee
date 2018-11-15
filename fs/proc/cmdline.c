LineageOS/android_kernel_samsung_msm8930-common

proc: Export androidboot.mode=charger if needed
Change-Id: I8ece47ce52681fa7e21d562a0266609675bb8f1a

fs/proc/cmdline.c
@@ -3,8 +3,19 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#ifdef CONFIG_SAMSUNG_LPM_MODE
+extern int poweroff_charging;
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_SAMSUNG_LPM_MODE
+	if (poweroff_charging) {
+		seq_printf(m, "%s %s\n", saved_command_line,
+				"androidboot.mode=charger");
+		return 0;
+	}
+#endif
 	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }

acroreiser/Koffee

fs/proc/cmdline.c
@@ -3,8 +3,19 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#ifdef CONFIG_BATTERY_SEC_U1
+extern int poweroff_charging;
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_BATTERY_SEC_U1
+	if (poweroff_charging) {
+		seq_printf(m, "%s %s\n", saved_command_line,
+				"androidboot.mode=charger");
+		return 0;
+	}
+#endif
 	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }