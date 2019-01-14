7ff002820ad277c9b931f40aedd7bc5db2fe93bb
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-14 11:14:04 +0300

    mali: disable GPU overclock by default

--- drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
+++ drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
@@ -804,6 +804,10 @@ mali_bool mali_dvfs_table_update(void)
 }
 #endif
 
+// Disable GPU overclock by default
+static unsigned int mali_use_5th_step = 0;
+module_param(mali_use_5th_step, uint, 0644);
+
 static unsigned int decideNextStatus(unsigned int utilization)
 {
 	static unsigned int level = 0;
@@ -817,11 +821,8 @@ static unsigned int decideNextStatus(unsigned int utilization)
 		if (utilization > (int)(255 * mali_dvfs[maliDvfsStatus.currentStep].upthreshold / 100) &&
 				level < MALI_DVFS_STEPS - 1) {
 			level++;
-#if 0 /* this prevents the usage of 5th step -gm */
-			if ((samsung_rev() < EXYNOS4412_REV_2_0) && 3 == get_mali_dvfs_status()) {
-				level=get_mali_dvfs_status();
-			}
-#endif
+			if (!mali_use_5th_step) /* this prevents the usage of 5th step -gm */
+				level = 3;
 		}
 		else if (utilization < (int)(255 * mali_dvfs[maliDvfsStatus.currentStep].downthreshold / 100) &&
 				level > 0) {
