4536e1303df46b1c38cc7bca2d946cec932f5f6f
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-20 03:00:44 +0100

    Revert "Revert "samsung_battery: Charge level interface""
    This reverts commit 738503b5be6760292d35e10a042687520771dcad.
    
    Change-Id: I649bc5b4140c9c84ec69dd3616b53209e90fd5f9

--- arch/arm/mach-exynos/charge_level.c
+++ arch/arm/mach-exynos/charge_level.c
@@ -35,6 +35,12 @@ static ssize_t charge_level_ac_store(struct kobject *kobj, struct kobj_attribute
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+<<<<<<< HEAD
+=======
+    if (ret != 1)
+        return -EINVAL;
+        
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > AC_CHARGE_LEVEL_MAX)
 		val = AC_CHARGE_LEVEL_MAX;
@@ -64,6 +70,12 @@ static ssize_t charge_level_usb_store(struct kobject *kobj, struct kobj_attribut
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+<<<<<<< HEAD
+=======
+    if (ret != 1)
+        return -EINVAL;
+        
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > USB_CHARGE_LEVEL_MAX)
 		val = USB_CHARGE_LEVEL_MAX;
@@ -92,6 +104,12 @@ static ssize_t charge_level_wireless_store(struct kobject *kobj, struct kobj_att
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+<<<<<<< HEAD
+=======
+    if (ret != 1)
+        return -EINVAL;
+        
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > WIRELESS_CHARGE_LEVEL_MAX)
 		val = WIRELESS_CHARGE_LEVEL_MAX;
@@ -121,6 +139,12 @@ static ssize_t ignore_unstable_power_store(struct kobject *kobj, struct kobj_att
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+<<<<<<< HEAD
+=======
+    if (ret != 1)
+        return -EINVAL;
+        
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 	// check whether value is valid
 	if ((val == 0) || (val == 1))
 		ignore_unstable_power = val;
@@ -144,6 +168,12 @@ static ssize_t ignore_safety_margin_store(struct kobject *kobj, struct kobj_attr
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+<<<<<<< HEAD
+=======
+    if (ret != 1)
+        return -EINVAL;
+        
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 	// check whether value is valid
 	if ((val == 0) || (val == 1))
 		ignore_safety_margin = val;
@@ -155,8 +185,15 @@ static ssize_t charge_info_show(struct kobject *kobj, struct kobj_attribute *att
 {
 
 	// print charge info
+<<<<<<< HEAD
 	return sprintf(buf, "%s / %d mA", charge_info_text, charge_info_level);
 
+=======
+	if (charge_info_level != 0)
+	    return sprintf(buf, "%s / %d mA", charge_info_text, charge_info_level);
+	else
+	    return sprintf(buf, "%s", charge_info_text);
+>>>>>>> 886ef1c6308b... Revert "Revert "samsung_battery: Charge level interface""
 }
 
 
