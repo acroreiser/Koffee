d69b228c1d6a1bfe3cf134a2db215c7432ad56f4
Jaroslav Zviezda <acroreiser@gmail.com>
2019-12-01 00:22:57 +0300

    Koffee: drop lots of old shit

--- anykernel_boeffla-i9300/ramdisk/koffee-early.sh
+++ anykernel_boeffla-i9300/ramdisk/koffee-early.sh
@@ -17,32 +17,24 @@
 # 2. Pyramid
 /sbin/busybox echo "pyramid" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-# 3. SELinux contexts
-/system/bin/restorecon -FRD /data/data
+# 3. * Dropped *
 
 # 4. zRam
 # Enable total 400 MB zRam on 1 device as default
 /sbin/busybox echo "1" > /sys/block/zram0/reset
-/sbin/busybox echo "209715200" > /sys/block/zram0/disksize
+/sbin/busybox echo "lz4" > /sys/block/zram1/comp_algorithm
+/sbin/busybox echo "419430400" > /sys/block/zram0/disksize
 /sbin/busybox mkswap /dev/block/zram0
-/sbin/busybox swapon /dev/block/zram0
-
-/sbin/busybox echo "1" > /sys/block/zram1/reset
-/sbin/busybox echo "lzo" > /sys/block/zram1/comp_algorithm
-/sbin/busybox echo "419430400" > /sys/block/zram1/disksize
-/sbin/busybox mkswap /dev/block/zram1
-/sbin/busybox swapon /dev/block/zram1
 /sbin/busybox echo "100" > /proc/sys/vm/swappiness
+/sbin/busybox swapon /dev/block/zram0
 
 # 5. BFQ and deadline
 /sbin/busybox echo "bfq" > /sys/block/mmcblk0/queue/scheduler
 /sbin/busybox echo "deadline" > /sys/block/mmcblk1/queue/scheduler
 
-# 6. Switch to fq_codel on mobile data and wlan
-/system/bin/tc qdisc add dev rmnet0 root fq_codel
-/system/bin/tc qdisc add dev wlan0 root fq_codel
+# 6. * Dropped *
 
-# 7. Enable network security enhacements from O
+# 7. Enable network security enhacements from Oreo
 /sbin/busybox echo 1 > /proc/sys/net/ipv4/conf/all/drop_unicast_in_l2_multicast
 /sbin/busybox echo 1 > /proc/sys/net/ipv6/conf/all/drop_unicast_in_l2_multicast
 /sbin/busybox echo 1 > /proc/sys/net/ipv4/conf/all/drop_gratuitous_arp
@@ -52,21 +44,17 @@
 /sbin/busybox echo 1 > /proc/sys/kernel/sched_child_runs_first
 
 # 9. Enlarge nr_requests for emmc
-/sbin/busybox echo 2048 > /sys/block/mmcblk0/queue/nr_requests
+/sbin/busybox echo 512 > /sys/block/mmcblk0/queue/nr_requests
 
 # 10. Sdcard buffer tweaks
 /sbin/busybox echo 2048 > /sys/block/mmcblk0/bdi/read_ahead_kb
 /sbin/busybox echo 1024 > /sys/block/mmcblk1/bdi/read_ahead_kb
 
-# 11. Strict request affinity for internal storage
-/sbin/busybox echo 2 > /sys/block/mmcblk0/queue/rq_affinity
+# 11. * Dropped *
 
-# 12. Try to speed up booting using readahead
-/system/bin/toybox readahead /system/lib/*.so
-/system/bin/toybox readahead /data/dalvik-cache/arm/*.dex
+# 12. * Dropped *
 
-# 13. FS Trim
-/sbin/busybox fstrim -v /data
+# 13. * Dropped *
 
 # 14. min free kbytes
 /sbin/busybox echo 40960 > /proc/sys/vm/min_free_kbytes
